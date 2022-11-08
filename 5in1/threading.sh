9d8b04e5ccdb83e8f2ff6a3e87e50a2c751687b9
Naseem Machlovi <naseem.machlovi@gmail.com>
2022-11-08 10:39:33 -0500

    multi_epoch

--- 5in1/threading.sh
+++ 5in1/threading.sh
@@ -9,8 +9,8 @@ do
 template=`cat "$1" | sed "s/@multi-pod/p$val/g ; s/@threading/$val/g" `
 echo "$template" | kubectl apply -f -
 #kubectl logs  p$val>> app.log## echo $template
-sleep 150
-kubectl logs --tail=1 p$val>> p$val.log
+# sleep 150
+# kubectl logs --tail=1 p$val>> p$val.log
 done
 
 IFS=$Field_Separator
